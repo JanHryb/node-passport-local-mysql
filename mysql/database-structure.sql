b6aaf02b84ae27bf88534d190082ae70f31789a4
JanHryb <jan.hryb@gmail.com>
2022-06-06 14:47:16 +0200

    made part of dashboard

--- mysql/database-structure.sql
+++ mysql/database-structure.sql
@@ -4,6 +4,7 @@ wyzwalacze
 triggery
 */
 
+#DATABASE STRUCTURE
 DROP DATABASE IF EXISTS warehouse;
 CREATE DATABASE warehouse;
 USE warehouse;
@@ -27,7 +28,7 @@ CREATE TABLE product_brands (product_brand_id INT AUTO_INCREMENT,
 CREATE TABLE products   (product_id INT AUTO_INCREMENT,
                         product_name VARCHAR(50) NOT NULL,
                         product_desc TEXT,
-                        product_price DECIMAL(5,2) NOT NULL,
+                        product_price DECIMAL(7,2) UNSIGNED NOT NULL,
                         product_amount INT UNSIGNED NOT NULL,
                         product_category_id INT NOT NULL,
                         product_brand_id INT NOT NULL,
@@ -38,10 +39,12 @@ CREATE TABLE products   (product_id INT AUTO_INCREMENT,
 CREATE TABLE orders (order_id INT AUTO_INCREMENT,
                     order_date DATE NOT NULL,
                     product_id INT NOT NULL,
-                    order_ordered_amount INT NOT NULL,
-                    order_total_price DECIMAL(5,2) NOT NULL,
+                    order_ordered_amount INT UNSIGNED NOT NULL,
+                    order_total_price DECIMAL(9,2) UNSIGNED NOT NULL,
+                    user_id INT NOT NULL,
                     PRIMARY KEY (order_id),
-                    FOREIGN KEY (product_id) REFERENCES products(product_id));
+                    FOREIGN KEY (product_id) REFERENCES products(product_id),
+                    FOREIGN KEY (user_id) REFERENCES users(user_id));
 
 # INSERTS
 INSERT INTO product_categories(product_category_name, product_category_desc)
@@ -53,6 +56,26 @@ VALUES ('adidas'), ('nike'), ('the north face'), ('wrangler'), ('MISBHV'), ('yee
 INSERT INTO products(product_name, product_desc,product_price, product_amount, product_category_id, product_brand_id)
 VALUES ('nike air force 1', 'immortal shoes', 250.00, 400 , 1, 2), ('yeezy boost 350''bone'' ', 'very comfy shoes', 800.00, 25, 1, 6);
 
+
+# PROCEDURES
+DELIMITER $$
+CREATE OR REPLACE PROCEDURE viewProducts()
+BEGIN
+    SELECT  products.product_id, products.product_name, products.product_desc, products.product_price, products.product_amount, product_categories.product_category_name, product_brands.product_brand_name
+	FROM products
+	INNER JOIN product_categories ON products.product_category_id = product_categories.product_category_id
+	INNER JOIN product_brands ON products.product_brand_id = product_brands.product_brand_id
+    ORDER BY product_id ASC ;
+END $$
+# CALL viewProducts();
+
+DELIMITER $$
+CREATE OR REPLACE PROCEDURE createProduct(product_name VARCHAR(50), product_desc TEXT, product_price DECIMAL(7,2) UNSIGNED, product_amount INT UNSIGNED, product_category_id INT, product_brand_id INT)
+BEGIN
+    INSERT INTO products(product_name, product_desc, product_price, product_amount, product_category_id, product_brand_id)
+    VALUES (product_name, product_desc, product_price, product_amount, product_category_id, product_brand_id);
+END $$
+# CALL createProduct('adidas forum low', '',350.00,50,1,6);
 
 
 
--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,10 +1,96 @@
 const express = require("express");
 const auth = require("../config/auth");
+const database = require("../config/database").connect();
 
 const router = express.Router();
 
 router.get("/", auth.isAuthenticated, (req, res) => {
   return res.status(200).render("dashboard/dashboard");
+});
+
+router.get("/products", auth.isAuthenticated, (req, res) => {
+  database.query(`CALL viewProducts();`, (err, viewResult) => {
+    if (err) {
+      console.error(err);
+    }
+    viewResult = viewResult[0];
+    database.query(
+      `SELECT product_category_id, product_category_name FROM product_categories;`,
+      (err, productCategories) => {
+        if (err) {
+          console.error(err);
+        }
+        database.query(
+          `SELECT product_brand_id, product_brand_name FROM product_brands;`,
+          (err, productBrands) => {
+            if (err) {
+              console.error(err);
+            }
+            return res.status(200).render("dashboard/products", {
+              viewResult,
+              productCategories,
+              productBrands,
+            });
+          }
+        );
+      }
+    );
+  });
+});
+
+router.post("/products", (req, res) => {
+  const {
+    product_name,
+    product_desc,
+    product_price,
+    product_amount,
+    product_category_id,
+    product_brand_id,
+  } = req.body;
+
+  if (
+    !(
+      product_name &&
+      product_price &&
+      product_amount &&
+      product_category_id &&
+      product_brand_id
+    )
+  ) {
+    req.flash("error", "please fill in all fields");
+    return res.status(400).redirect("/dashboard/products");
+  }
+  if (product_name.length < 3) {
+    req.flash("error", "product name should contain at least 3 characters");
+    return res.status(400).redirect("/dashboard/products");
+  }
+  if (parseFloat(product_price) < 0) {
+    req.flash("error", "product price can't be lower than 0");
+    return res.status(400).redirect("/dashboard/products");
+  }
+  if (parseFloat(product_price) > 99999.99) {
+    req.flash("error", "product price can't be higher than 99999.99");
+    return res.status(400).redirect("/dashboard/products");
+  }
+  if (parseFloat(product_amount) < 0) {
+    req.flash("error", "product amount can't be lower than 0");
+    return res.status(400).redirect("/dashboard/products");
+  }
+
+  database.query(
+    `CALL createProduct('${product_name}', '${product_desc}', ${parseFloat(
+      product_price
+    )}, ${parseInt(product_amount)}, ${parseInt(
+      product_category_id
+    )}, ${parseInt(product_brand_id)});`,
+    (err, result) => {
+      if (err) {
+        console.error(err);
+      }
+      req.flash("success", "one record has been added");
+      return res.status(201).redirect("/dashboard/products");
+    }
+  );
 });
 
 module.exports = router;
